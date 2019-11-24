1754be9ee80f16f9579aba4a802684937f232618
James Hibben <jmhibben@gmail.com>
2019-11-23 18:33:31 -0600

    FIX: Test init now properly imports parent package

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,13 @@
-from ..pandocwrapper import *
-from .scan import *
-from .stringify import *
-from .verify import *
+#from .scan import *
+#from .stringify import *
+#from .verify import *
+
+# Make parent package modules available
+from sys import path
+from os import getcwd
+
+path.append(getcwd() + '/..')
+from pandocwrapper import *
+
+# Make test modules available
 from .convert import *
