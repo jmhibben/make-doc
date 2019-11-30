25b8e8cf92e12344092f2a9cf2b1de1a37e626d7
James Hibben <jmhibben@gmail.com>
2019-11-30 15:22:18 -0600

    CHORE: Removed print statement

--- tests/cli.py
+++ tests/cli.py
@@ -4,5 +4,4 @@ import subprocess
 class CLITests(unittest.TestCase):
     def testBasicConvert(self):
         results = subprocess.run(['python3', 'makestory.py', 'tests/static/readme.docx'], stdout=subprocess.PIPE)
-        print(results)
         self.assertEqual(results.returncode, 0)
