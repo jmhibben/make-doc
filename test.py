2ea6c5f16bcd65198aea08a575c5108b4ad95ded
James Hibben <jmhibben@gmail.com>
2019-12-01 18:35:05 -0600

    TEST: Added logging setup to test module

--- test.py
+++ test.py
@@ -1,5 +1,15 @@
 import unittest
 import tests
+import logging as log
+
+def main():
+    log.basicConfig(
+            format='%(levelname)s %(asctime)s %(name)s: %(message)s',
+            datefmt='%y/%m/%d %H:%M:%S%p',
+            level=log.DEBUG
+            )
+    logger = log.getLogger(__name__)
+    unittest.main(module='tests', verbosity=2)
 
 if __name__ == '__main__':
-    unittest.main(module='tests', verbosity=2)
+    main()
