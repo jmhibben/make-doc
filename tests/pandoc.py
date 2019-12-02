db66029a1ae4ef47afb2243ecf13cd223fdc4a37
James Hibben <jmhibben@gmail.com>
2019-12-01 18:36:08 -0600

    TEST: Fixed tests to follow new API

--- tests/pandoc.py
+++ tests/pandoc.py
@@ -4,13 +4,14 @@ import unittest
 class PandocWrapperTests(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.pandoc = PandocWrapper()
+        cls.pandoc = PandocWrapper({'meta': 'meta.json'})
 
+    @unittest.skip("Shouldn't be calling scan anymore")
     def testScan(self):
         expected = ['./README.md']
         contents = self.pandoc.scan()
         self.assertEqual(contents, expected)
 
     def testConvert(self):
-        completed = self.pandoc.convert('./README.docx')
+        completed = self.pandoc.convert()
         self.assertEqual(completed.returncode, 0)
